7d7cff63eaee51429f609d43f2d02bd2dc732e43
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-07-13 13:26:20 +0530

    created custom hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import useFetchData from "./hooks/useFetchData";
 
 function App() {
+    const res = useFetchData("https://mekvahan.com/api/android_intern_task", {
+        mobile: "9999999999",
+        password: "0123456789",
+    });
+
+    console.log("here", res);
+
     return <div className="App">Hello, World</div>;
 }
 
---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+export default function useFetchData(url, options) {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("fetchdata");
+
+            await axios
+                .post(url, options)
+                .then((res) => {
+                    console.log(res);
+                    console.log("data", res.data.status);
+                    setResponse(res.data.status);
+                })
+                .catch((err) => {
+                    console.log("err", err);
+                    setError(true);
+                });
+        };
+
+        fetchData();
+    }, []);
+    return { response, error };
+}
